5d73a37222085239bd9ef6e70a986c3099c584ab
Samkiroko <kirokodev@gmail.com>
2023-04-25 17:35:42 +0300

    Creating service layer methods

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,5 +1,6 @@
+// In src/v1/routes/workoutRoutes.js
 const express = require('express');
-const workoutController = require('../controllers/workoutController');
+const workoutController = require('../../controllers/workoutController');
 
 const router = express.Router();
 

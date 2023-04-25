b2accf192dc9cde83bcce924a6e59a0b7d72d610
Samkiroko <kirokodev@gmail.com>
2023-04-25 20:20:44 +0300

    Documenting the Api with swagger

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 // *** ADD ***
 const apicache = require('apicache');
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
+const { swaggerDocs: V1SwaggerDocs } = require('./v1/swagger');
 
 const app = express();
 // *** ADD ***
@@ -17,4 +18,5 @@ app.use('/api/v1/workouts', v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });

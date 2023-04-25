c66272f2db45ecc43ee6f89b965d3c2c314512d4
Samkiroko <kirokodev@gmail.com>
2023-04-25 20:11:37 +0300

    Improving the functionality and adding caching

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,9 +1,9 @@
-// In src/controllers/workoutController.js
 const workoutService = require('../services/workoutService');
 
 const getAllWorkouts = (req, res) => {
+  const { mode } = req.query;
   try {
-    const allWorkouts = workoutService.getAllWorkouts();
+    const allWorkouts = workoutService.getAllWorkouts({ mode });
     res.send({ status: 'OK', data: allWorkouts });
   } catch (error) {
     res
@@ -16,12 +16,15 @@ const getOneWorkout = (req, res) => {
   const {
     params: { workoutId },
   } = req;
+
   if (!workoutId) {
     res.status(400).send({
       status: 'FAILED',
       data: { error: "Parameter ':workoutId' can not be empty" },
     });
+    return;
   }
+
   try {
     const workout = workoutService.getOneWorkout(workoutId);
     res.send({ status: 'OK', data: workout });
@@ -34,6 +37,7 @@ const getOneWorkout = (req, res) => {
 
 const createNewWorkout = (req, res) => {
   const { body } = req;
+
   if (
     !body.name ||
     !body.mode ||
@@ -48,8 +52,8 @@ const createNewWorkout = (req, res) => {
           "One of the following keys is missing or is empty in request body: 'name', 'mode', 'equipment', 'exercises', 'trainerTips'",
       },
     });
-    return;
   }
+
   const newWorkout = {
     name: body.name,
     mode: body.mode,
@@ -57,13 +61,14 @@ const createNewWorkout = (req, res) => {
     exercises: body.exercises,
     trainerTips: body.trainerTips,
   };
+
   try {
     const createdWorkout = workoutService.createNewWorkout(newWorkout);
     res.status(201).send({ status: 'OK', data: createdWorkout });
   } catch (error) {
     res
       .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
+      .send({ status: 'FAILDED', data: { error: error?.message || error } });
   }
 };
 
@@ -72,12 +77,14 @@ const updateOneWorkout = (req, res) => {
     body,
     params: { workoutId },
   } = req;
+
   if (!workoutId) {
     res.status(400).send({
       status: 'FAILED',
       data: { error: "Parameter ':workoutId' can not be empty" },
     });
   }
+
   try {
     const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
     res.send({ status: 'OK', data: updatedWorkout });
@@ -92,12 +99,14 @@ const deleteOneWorkout = (req, res) => {
   const {
     params: { workoutId },
   } = req;
+
   if (!workoutId) {
     res.status(400).send({
       status: 'FAILED',
       data: { error: "Parameter ':workoutId' can not be empty" },
     });
   }
+
   try {
     workoutService.deleteOneWorkout(workoutId);
     res.status(204).send({ status: 'OK' });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,12 +1,20 @@
 // In src/v1/routes/workoutRoutes.js
 const express = require('express');
+// *** ADD ***
+const apicache = require('apicache');
 const workoutController = require('../../controllers/workoutController');
+const recordController = require('../../controllers/recordController');
 
 const router = express.Router();
+// *** ADD ***
+const cache = apicache.middleware;
 
-router.get('/', workoutController.getAllWorkouts);
+// *** ADD ***
+router.get('/', cache('2 minutes'), workoutController.getAllWorkouts);
 
 router.get('/:workoutId', workoutController.getOneWorkout);
+
+router.get('/:workoutId/records', recordController.getRecordForWorkout);
 
 router.post('/', workoutController.createNewWorkout);
 
